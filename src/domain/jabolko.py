5a927127c75cffb1b1aa3706c4b15f67cec19cdf
Mark <mare1.loborec@gmail.com>
2023-03-29 16:02:26 +0200

    Preimenoval sem hrano v jabolko.

--- src/domain/jabolko.py
+++ src/domain/jabolko.py
@@ -1,4 +1,4 @@
-class Hrana:
+class Jabolko:
     def __init__(self, size: float, x: float, y: float):
         self.size: float = size
         self.x: float = x
--- src/domain/svet.py
+++ src/domain/svet.py
@@ -1,8 +1,8 @@
-from src.domain.hrana import Hrana
+from src.domain.jabolko import Jabolko
 from src.domain.snake import Snake
 import random
 
 class Svet:
     def __init__(self):
         self.snake = Snake()
-        self.hrana = Hrana(size=0.1,x=random.randint(0,10)/10,y=random.randint(0,10)/10)
+        self.hrana = Jabolko(size=0.1, x=random.randint(0, 10) / 10, y=random.randint(0, 10) / 10)
